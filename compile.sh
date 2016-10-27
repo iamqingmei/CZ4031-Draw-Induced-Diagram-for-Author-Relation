5f60ccb149d26edcce6fc1bad69cde5f03fa7f00
iamqingmei <Ageha@Qingmeis-MacBook-Air.local>
2016-10-27 14:26:42 +0800

    init

---
+++ compile.sh
@@ -0,0 +1,2 @@
+d=`pwd`
+export CLASSPATH=.:$d/Library/antlr4-runtime-4.5.3.jar:$d/Library/jgraph-5.13.0.0.jar:$d/Library/jgrapht-core-1.0.0.jar:$d/Library/jgrapht-demo-1.0.0.jar:$d/Library/jgrapht-ext-1.0.0-uber.jar:$d/Library/jgrapht-ext-1.0.0.jar:$d/Library/jgraphx-2.0.0.1.jar;
